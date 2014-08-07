d2faf83bc19020d2be3d7889f066f1c38bfb83e0
CodeHex <codehex@AirBook.local>
2014-08-08 00:04:49 +0900

    Yo.pl
    Send yo or yoall and get your yo api received yo count.

---
+++ Yo.pl
@@ -0,0 +1,80 @@
+#! /usr/bin/perl
+use strict; #おまじない
+use warnings; #おまじない
+use LWP::UserAgent;
+use HTTP::Request::Common;
+use HTTP::Response;
+use URI;
+
+die &usage if (@ARGV && $ARGV[0] =~ /^-h/); # options help
+die &version if (@ARGV && $ARGV[0] =~ /^-v/); # version
+
+my $ua = LWP::UserAgent->new;
+my $http = HTTP::Response->new;
+
+my $api_key = ""; # Yo api key
+
+my $url = 'http://api.justyo.co';
+my %list = ("all" => "/yoall/",
+			"yo" => "/yo/",
+			"count" => "/subscribers_count/");
+my ($api,$res);
+			if(@ARGV&&$ARGV[1]){
+			my $send_user = @ARGV&&$ARGV[1]? $ARGV[1] : " "; # input default user
+
+			$api = $url.$list{'yo'};
+			$res = $ua->request(POST $api,
+			Content_Type => 'application/x-www-form-urlencoded', #Definitely Necessary
+			Content      =>[ api_token => $api_key,
+         					 username  => $send_user ]);
+         	} elsif(@ARGV&&$ARGV[0] eq "all"){
+         	$api = $url.$list{'all'};
+         	$res = $ua->request(POST $api,
+         	Content_Type => 'application/x-www-form-urlencoded', #Definitely Necessary
+         	Content      =>[ api_token => $api_key ]);
+			} elsif(@ARGV&&$ARGV[0] eq "count"){
+			$api = $url.$list{'count'};
+			my $uri = URI->new($api);
+			$uri->query_form(api_token => $api_key);
+			$res = $ua->request(GET $uri);
+			} else {die &usage;}
+
+	print $res->content;
+
+
+
+sub usage {
+      return <<__EOF__
+Usage: % Yo.pl [OPTIONS] [USERNAME]
+
+--- Input messages example ---
+  % Yo.pl all   		<- yo to all users
+  % Yo.pl count			<- yo count
+  % Yo.pl yo CodeHex		<- yo to single user
+--- Options ---
+    -h         : help
+    -v         : version
+    yo         : yo to single user
+    all        : yo to all users
+    count      : yo count
+__EOF__
+}
+
+sub version {
+    return <<__EOF__
+
+  ___    ___ ________     
+ |\\  \\  /  /|\\   __  \\    
+ \\ \\  \\/  / | \\  \\|\\  \\   
+  \\ \\    / / \\ \\   \\\\  \\  
+   \\/  /  /   \\ \\   \\\\  \\ 
+ __/  / /      \\ \\_______\\
+|\\___/ /        \\|_______|
+\\|___|/                   
+                          
+                          
+Version: Yo.pl v1.0
+Create by CodeHex(K) http://twitter.com/CodeHex
+My Yo id: CODEHEX 
+__EOF__
+}
